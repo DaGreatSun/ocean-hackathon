08d584923cd0ba5b448f4976cad6d8b2a11fa99c
chancheesun <cheesysun@gmail.com>
2023-11-17 04:51:29 +0800

    Added icon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Ocean Hackathon",
   description: "Ocean Hackathon",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
