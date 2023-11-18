b0d228d3b854de82462ebdaaff261f03a8cecb52
chancheesun <cheesysun@gmail.com>
2023-11-18 10:38:33 +0800

    add background

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,9 @@ module.exports = {
     },
   ],
   theme: {
+    backgroundImage: {
+      loginBG: "url('../public/assets/img/top.jpg')",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
