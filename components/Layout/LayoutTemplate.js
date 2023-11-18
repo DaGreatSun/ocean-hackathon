07af3cfb15763d484874083f4b2bb4eb864facb8
chancheesun <cheesysun@gmail.com>
2023-11-18 23:12:29 +0800

    Footer done

--- components/Layout/LayoutTemplate.js
+++ components/Layout/LayoutTemplate.js
@@ -1,4 +1,9 @@
-import { FaFacebook, FaInstagram, FaPhone } from "react-icons/fa6";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLocationPin,
+  FaPhone,
+} from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 export default function LayoutTemplate({ children }) {
@@ -19,8 +24,42 @@ export default function LayoutTemplate({ children }) {
       <div className="grid grid-cols-1 bg-[#39729e] lg:grid-cols-2">
         <div className="col-span-1 lg:border-r-2 lg:border-r-black">
           <h1 className="py-6 text-center text-2xl text-white">Contact Us</h1>
-          <div className="flex place-content-center items-center">
+          <div className="flex place-content-center items-center pb-7">
             <div className="grid grid-cols-1">
+              <div className="col-span-1 inline-flex items-center">
+                <span className="p-2">
+                  <FaLocationPin fill="#ffffff" size={20} />
+                </span>
+                <span className="w-96 text-white">
+                  34, Jalan Dutamas Raya, Segambut, 52100 Kuala Lumpur, Wilayah
+                  Persekutuan Kuala Lumpur
+                </span>
+              </div>
+              <div className="col-span-1 inline-flex items-center">
+                <span className="p-2">
+                  <MdEmail fill="#ffffff" size={20} />
+                </span>
+                <span className="text-white">oceanhackathonkl@gmail.com</span>
+              </div>
+              <div className="col-span-1 inline-flex items-center">
+                <span className="p-2">
+                  <FaPhone fill="#ffffff" size={20} />
+                </span>
+                <span className="text-white">+60 12-345 6789</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 lg:border-r-2 lg:border-r-black">
+          <h1 className="py-6 text-center text-2xl text-white">Socials</h1>
+          <div className="flex place-content-center items-center pb-7">
+            <div className="grid grid-cols-1">
+              <div className="col-span-1 inline-flex items-center">
+                <span className="p-2">
+                  <FaPhone fill="#ffffff" size={20} />
+                </span>
+                <span className="text-white">+60 12-345 6789</span>
+              </div>
               <div className="col-span-1 inline-flex items-center">
                 <span className="p-2">
                   <FaFacebook fill="#ffffff" size={20} />
@@ -33,17 +72,8 @@ export default function LayoutTemplate({ children }) {
                 </span>
                 <span className="text-white">@ocean_hackathon_kl</span>
               </div>
-              <div className="col-span-1 inline-flex items-center">
-                <span className="p-2">
-                  <MdEmail fill="#ffffff" size={20} />
-                </span>
-                <span className="text-white">oceanhackathonkl@gmail.com</span>
-              </div>
             </div>
           </div>
-        </div>
-        <div className="col-span-1 border-t-2 border-t-black lg:border-t-0 ">
-          <h1 className="py-6 text-center text-2xl text-white">Contact Us</h1>
         </div>
       </div>
     </>
